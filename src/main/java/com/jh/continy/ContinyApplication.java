2d7271af8ecf95286812de93a04460ffd4cce491
huendan05 <593901335@qq.com>
2021-04-02 15:23:04 +0800

    frist

---
+++ src/main/java/com/jh/continy/ContinyApplication.java
@@ -0,0 +1,13 @@
+package com.jh.continy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContinyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContinyApplication.class, args);
+	}
+
+}
